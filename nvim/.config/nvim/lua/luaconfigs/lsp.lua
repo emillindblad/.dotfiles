10b7c72ec4282e28e397dbdc95f256468989421a
emillindblad <emil@lindblad.tech>
2022-09-21 17:15:16 +0200

    added emmet

--- nvim/.config/nvim/lua/luaconfigs/lsp.lua
+++ nvim/.config/nvim/lua/luaconfigs/lsp.lua
@@ -54,3 +54,15 @@ require("lspconfig").tsserver.setup(config())
 
 --tailwind
 require'lspconfig'.tailwindcss.setup(config())
+
+require'lspconfig'.emmet_ls.setup(config({
+    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    init_options = {
+      html = {
+        options = {
+          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+}))
