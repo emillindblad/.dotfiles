463fc44c994ddf0bf99e664c5953695c37638fe7
emillindblad <emil@lindblad.tech>
2022-10-01 20:06:22 +0200

    updated config for 1440p monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -9,11 +9,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch Polybar on all monitors, using default config location ~/.config/polybar/config
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload mybar &
-  done
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        if [ $m == 'DP-0' ]
+        then
+            MONITOR=$m polybar --reload 1440pbar &
+        else
+            MONITOR=$m polybar --reload mybar &
+        fi
+    done
 else
-  polybar --reload mybar &
+    polybar --reload mybar &
 fi
 
 echo "Polybar launched..."
