1ec19dd97eefb60efbcff81a8e804d405ef1a7a4
emillindblad <emil@lindblad.tech>
2023-06-26 20:12:24 +0200

    feat: Changed lsp configs

--- nvim/.config/nvim/lua/luaconfigs/lsp.lua
+++ nvim/.config/nvim/lua/luaconfigs/lsp.lua
@@ -1,46 +1,87 @@
 require("mason").setup()
-require("mason-lspconfig").setup {
-    ensure_installed = { "pyright", "texlab", "clangd", "jdtls", "tsserver", "tailwindcss", "eslint", "emmet_ls", "rust_analyzer" }
+local mason_lspconfig = require("mason-lspconfig")
+local servers = {
+    pyright = {},
+    texlab = {},
+    clangd = {
+        cmd = { "clangd", "--background-index", "--clang-tidy" },
+        root_dir = function() return vim.loop.cwd() end
+    },
+    jdtls = {},
+    tsserver = {},
+    tailwindcss = {},
+    eslint = {},
+    emmet_ls = {
+        filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+        init_options = {
+            html = {
+                options = {
+                    -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+                    ["bem.enabled"] = true,
+                },
+            },
+        }
+    },
+    rust_analyzer = {},
+    lua_ls = {
+        Lua = {
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = {'vim'},
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false,
+            },
+            telemetry = { enable = false, },
+        },
+    }
 }
 
-local util = require 'lspconfig.util'
-
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    update_in_insert = true,
-    })
-
-local function config(_config)
-    return vim.tbl_deep_extend("force", {
-        --capabilities = require('cmp_nvim_lsp').default_capabilites(vim.lsp.protocol.make_client_capabilities())
-    }, _config or {})
+local on_attach = function(_, bufnr)
+-- TODO: Add bindings for buffers with servers attached
 end
 
---Python
-require('lspconfig').pyright.setup{}
+local util = require('lspconfig.util')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
---LaTeX
-require('lspconfig').texlab.setup(config())
+vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        update_in_insert = true,
+    }
+)
 
--- C/C++
-require'lspconfig'.clangd.setup(config({
-    cmd = { "clangd", "--background-index", "--clang-tidy" },
-    root_dir = function() return vim.loop.cwd() end
-}))
-
---Javuh
-local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
-local workspace_dir = '/home/emil/.local/share/nvim/lsp_servers/jdtls/bin/workspace/' .. project_name
-require'lspconfig'.jdtls.setup{
-    cmd = {
-        'java',
-        '-data', workspace_dir
-    },
+mason_lspconfig.setup {
+    ensure_installed = vim.tbl_keys(servers)
 }
 
+mason_lspconfig.setup_handlers {
+    function (server_name)
+        require('lspconfig')[server_name].setup {
+            capabilities = capabilities,
+            on_attach = on_attach,
+            settings = servers[server_name],
+        }
+    end,
+}
+
+-- TODO: Setup for remaining servers.
+--Javuh
+--local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
+--local workspace_dir = '/home/emil/.local/share/nvim/lsp_servers/jdtls/bin/workspace/' .. project_name
+--require'lspconfig'.jdtls.setup{
+    --cmd = {
+        --'java',
+        --'-data', workspace_dir
+    --},
+--}
+
 --SQL
-require'lspconfig'.sqlls.setup(config({
-    root_dir = util.root_pattern '.sqllsrc.json'
-}))
+--require'lspconfig'.sqlls.setup(config({
+    --root_dir = util.root_pattern '.sqllsrc.json'
+--}))
 
 --cssls
 --Enable (broadcasting) snippet capability for completion
@@ -50,28 +91,3 @@ require'lspconfig'.sqlls.setup(config({
 --require'lspconfig'.cssls.setup {
   --capabilities = capabilities,
 --}
-
---tsserver
-require("lspconfig").tsserver.setup(config())
-
---tailwind
-require'lspconfig'.tailwindcss.setup(config())
-
--- ESLint
-require'lspconfig'.eslint.setup{config()}
-
--- emmet_ls
-require'lspconfig'.emmet_ls.setup(config({
-    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-    init_options = {
-      html = {
-        options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
-          ["bem.enabled"] = true,
-        },
-      },
-    }
-}))
-
---rust
-require('lspconfig').rust_analyzer.setup{}
