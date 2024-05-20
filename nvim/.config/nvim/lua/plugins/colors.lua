48c789f90d927a53d56f44ddddb10afdb6aa32b5
Emil Lindblad <emil@lindblad.tech>
2024-05-20 20:47:24 +0200

    feat(nvim): change lualine to gruvbox colors

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -10,29 +10,17 @@ return {
         operators = false,
         folds = false,
       },
-      invert_selection = false,
       contrast = 'hard',
       transparent_mode = true,
       overrides = {
-        --LineNr = {fg = "#83a598"},
-        netrwDir = { fg = '#83a598' },
         TelescopeBorder = { fg = '#7c6f64' },
-        --qfFileName = {fg = "#aed75f"},
-        --QuickFixLine = { bg = "#665c54" },
-        LspInfoBorder = { fg = '#EBDBB2' },
-        TreesitterContext = { bg = '#504945' },
+        QuickFixLine = { bg = '#504945' },
+        LspInfoBorder = { fg = '#ebdbb2' },
+        TreesitterContext = { bg = '#504945' }, -- Transparent by default
       },
     }
 
-    --[[ vim.api.nvim_create_autocmd("FileType", {
-      pattern = "mason",
-      callback = function ()
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "#EBDBB2" })
-      end
-    }) ]]
-
-    --Bg for non transparent mode "#292F3A"
-
+    --Bg for non transparent mode '#282828'
     vim.o.background = 'dark'
     vim.cmd([[colorscheme gruvbox]])
   end,
--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -5,46 +5,46 @@ return {
   },
   config = function()
     local colors = {
-      color0 = '#444444',
-      color1 = '#f2c68a',
-      color2 = '#d0d0d0',
-      color3 = '#585858',
-      color6 = '#353535',
-      color7 = '#e5786d',
-      color10 = '#95e454',
-      color13 = '#8ac6f2',
-      color14 = '#a8a8a8',
-      color15 = '#969696',
-      color16 = '#666666',
+      bg0_h = '#1d2021',
+      bg0_s = '#32302f',
+      bg1 = '#3c3836',
+      bg2 = '#504945',
+      fg2 = '#d5c4a1',
+      fg3 = '#bdae93',
+      red = '#fb4934',
+      green = '#98971a',
+      blue = '#83a598',
+      orange = '#fe8019',
     }
 
-    local wombat = {
+    local gruvbat = {
       visual = {
-        a = { fg = colors.color0, bg = colors.color1 },
-        b = { fg = colors.color2, bg = colors.color3 },
+        a = { fg = colors.bg0_h, bg = colors.orange },
+        b = { fg = colors.fg2, bg = colors.bg2 },
       },
       replace = {
-        a = { fg = colors.color6, bg = colors.color7 },
-        b = { fg = colors.color2, bg = colors.color3 },
+        a = { fg = colors.bg0_h, bg = colors.red },
+        b = { fg = colors.fg2, bg = colors.bg2 },
       },
       insert = {
-        a = { fg = colors.color0, bg = colors.color10 },
-        b = { fg = colors.color2, bg = colors.color3 },
+        a = { fg = colors.bg0_h, bg = colors.green },
+        b = { fg = colors.fg2, bg = colors.bg2 },
       },
       normal = {
-        a = { fg = colors.color0, bg = colors.color13 },
-        b = { fg = colors.color2, bg = colors.color3 },
-        c = { fg = colors.color14, bg = colors.color0 },
+        a = { fg = colors.bg0_h, bg = colors.blue },
+        b = { fg = colors.fg2, bg = colors.bg2 },
+        c = { fg = colors.fg3, bg = colors.bg1 },
       },
       inactive = {
-        a = { fg = colors.color15, bg = colors.color0 },
-        b = { fg = colors.color16, bg = colors.color6 },
-        c = { fg = colors.color15, bg = colors.color6 },
+        a = { fg = colors.fg3, bg = colors.bg0_s },
+        b = { fg = colors.fg3, bg = colors.bg0_s },
+        c = { fg = colors.fg3, bg = colors.bg0_s },
       },
     }
+
     require('lualine').setup {
       options = {
-        theme = wombat,
+        theme = gruvbat,
         component_separators = { left = '|', right = '|' },
       },
       sections = {
