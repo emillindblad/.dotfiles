839fd7238ff71453bdf707046379d8e97788901d
Emil Lindblad <emil@lindblad.tech>
2024-06-23 15:37:50 +0200

    feat(wezterm): font size on desktop

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -11,5 +11,9 @@ config.window_background_opacity = 0.9
 config.font = wezterm.font('Source Code Pro', { weight = 'Medium' })
 -- config.font = wezterm.font('Monocraft', { weight = 'Medium' })
 
+if wezterm.hostname() == 'endor' then
+  config.font_size = 13.2
+end
+
 config.enable_tab_bar = false
 return config
